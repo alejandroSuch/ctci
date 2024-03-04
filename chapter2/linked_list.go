d8dc8dbaa0027eda7da1795413e559af147a878a
Alejandro Such <alejandro.such@gitkraken.com>
2024-03-04 07:03:18 +0100

    ✨ Add new stuff to the linked list

--- chapter2/linked_list.go
+++ chapter2/linked_list.go
@@ -2,6 +2,54 @@ package chapter2
 
 import "reflect"
 
+type LinkedList[T interface{}] struct {
+	N      *Node[T]
+	Length int
+}
+
+func (l *LinkedList[T]) Append(data T) {
+	if l.N == nil {
+		l.N = NewNode(data)
+	} else {
+		l.N.Append(data)
+	}
+
+	l.Length = l.Length + 1
+}
+
+func (l *LinkedList[T]) Reverse() LinkedList[T] {
+	n := l.N
+	if n == nil {
+		return NewLinkedList[T](nil)
+	}
+
+	var result *Node[T]
+
+	for n != nil {
+		aux := NewNode(n.Data)
+		aux.Next = result
+		result = aux
+		n = n.Next
+	}
+
+	return NewLinkedList(result)
+}
+
+func NewLinkedList[T interface{}](n *Node[T]) LinkedList[T] {
+	length := 0
+	aux := n
+	if aux != nil {
+		for aux != nil {
+			aux = aux.Next
+			length = length + 1
+		}
+	}
+	return LinkedList[T]{
+		N:      n,
+		Length: length,
+	}
+}
+
 type Node[T interface{}] struct {
 	Next *Node[T]
 	Data T
